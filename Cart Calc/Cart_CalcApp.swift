746cb38e00b5c1891e544bc56a509a47f83be757
dadaek <dadaek@naver.com>
2026-01-10 01:05:45 +0900

    Initial Commit

---
+++ Cart Calc/Cart_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cart_CalcApp.swift
+//  Cart Calc
+//
+//  Created by 이다은 on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Cart_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cart Calc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cart Calc
+//
+//  Created by 이다은 on 1/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
